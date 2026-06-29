fe702da71c53f5769bbd3aa8389ee4bd0e6863bb
ilham <ilham@ilhams-MacBook-Air-2.local>
2026-06-29 10:03:39 +0700

    Fix Vercel build: Add missing assets and data files

---
+++ src/data/countries.js
@@ -0,0 +1,17 @@
+export const COUNTRIES = [
+  { "cca2": "ID", "name": "Indonesia", "idd": "+62", "flag": "https://flagcdn.com/id.svg" },
+  { "cca2": "AU", "name": "Australia", "idd": "+61", "flag": "https://flagcdn.com/au.svg" },
+  { "cca2": "CN", "name": "China", "idd": "+86", "flag": "https://flagcdn.com/cn.svg" },
+  { "cca2": "FR", "name": "France", "idd": "+33", "flag": "https://flagcdn.com/fr.svg" },
+  { "cca2": "DE", "name": "Germany", "idd": "+49", "flag": "https://flagcdn.com/de.svg" },
+  { "cca2": "IN", "name": "India", "idd": "+91", "flag": "https://flagcdn.com/in.svg" },
+  { "cca2": "JP", "name": "Japan", "idd": "+81", "flag": "https://flagcdn.com/jp.svg" },
+  { "cca2": "MY", "name": "Malaysia", "idd": "+60", "flag": "https://flagcdn.com/my.svg" },
+  { "cca2": "PH", "name": "Philippines", "idd": "+63", "flag": "https://flagcdn.com/ph.svg" },
+  { "cca2": "SG", "name": "Singapore", "idd": "+65", "flag": "https://flagcdn.com/sg.svg" },
+  { "cca2": "KR", "name": "South Korea", "idd": "+82", "flag": "https://flagcdn.com/kr.svg" },
+  { "cca2": "TH", "name": "Thailand", "idd": "+66", "flag": "https://flagcdn.com/th.svg" },
+  { "cca2": "GB", "name": "United Kingdom", "idd": "+44", "flag": "https://flagcdn.com/gb.svg" },
+  { "cca2": "US", "name": "United States", "idd": "+1", "flag": "https://flagcdn.com/us.svg" },
+  { "cca2": "VN", "name": "Vietnam", "idd": "+84", "flag": "https://flagcdn.com/vn.svg" }
+];
